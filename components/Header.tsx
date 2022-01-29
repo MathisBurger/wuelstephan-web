ecf6f39f3d2dcd2100ecfb00381d4894b16becb1
Mathis <handymathis@gmail.com>
2022-01-29 18:53:34 +0100

    [TASK] fixed header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import style from "../styles/Header.module.css";
 
 
 const Header: React.FC = () => {
 
+    const [pageScrolled, setPageScrolled] = useState<boolean>(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 0) {
+                console.log("kanalzerstöer");
+                setPageScrolled(true);
+            } else {
+                setPageScrolled(false);
+            }
+        });
+    }, []);
+
     return (
-        <div className={style.header}>
+        <div className={`${style.header} ${pageScrolled ? style.headerScrolled : ''}`}>
             <img src="/logo.png" alt="logo" style={{height: '120px'}} />
         </div>
     );
