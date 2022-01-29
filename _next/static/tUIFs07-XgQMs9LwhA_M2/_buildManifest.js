2048c5f33be09cc4394b675055f3b773e1e1b51a
MathisBurger <57145815+MathisBurger@users.noreply.github.com>
2022-01-29 18:12:30 +0000

    Deploying to gh-pages from @ MathisBurger/wuelstephan-web@7ff90c539ec590e16aaa99da76ef6777d8f56f77 🚀

---
+++ _next/static/tUIFs07-XgQMs9LwhA_M2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/3ccc0380ecc16d75.css","static/chunks/pages/index-6bed27a0eaf4994c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
