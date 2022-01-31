5b2add3bf20af8af3824ab6e1904fc8b688cee7f
Mathis <handymathis@gmail.com>
2022-01-31 19:49:20 +0100

    [TASK] added about us title

---
+++ components/AboutUsComponent.tsx
@@ -0,0 +1,17 @@
+import style from "../styles/aboutUs.module.css";
+
+const AboutUsComponent = () => {
+
+    return (
+      <div className={style.container}>
+        <div className={style.title}>SC Wühlstephan</div>
+          <div className={style.text}>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+
+              Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,
+          </div>
+      </div>
+    );
+}
+
+export default AboutUsComponent;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,15 @@ import type { NextPage } from 'next'
 import Header from "../components/Header";
 import style from "../styles/Index.module.css";
 import MemberComponent from "../components/MemberComponent";
+import AboutUsComponent from "../components/AboutUsComponent";
 
 const Home: NextPage = () => {
-
-
   return (
     <>
       <Header />
       <div className={style.container}>
           <img src="/headerImg.jpeg" className={style.topImg} />
+          <AboutUsComponent />
           <MemberComponent />
       </div>
     </>
